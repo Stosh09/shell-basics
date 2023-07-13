5f8ff5bb6f1fa34bb7aa7424a26e33994bc42dac
OukoFranchez <oukofranchez@gmail.com>
2023-07-13 13:15:34 +0300

    get line of stdin

---
+++ get_line.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+
+int main(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+
+	getline(&line, &len, stdin);
+	printf("The line of text is: %s\n", line);
+	printf("The no of bytes of %s exc nul ptr = %ld\n", line, len);
+	free(line);
+	return (0);
+
+}
